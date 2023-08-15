fcc64566cc859b9469a005142676bdde5839b65b
Tobías Oviedo <76019160+Tobias388@users.noreply.github.com>
2023-08-15 19:04:38 -0300

    Add files via upload

---
+++ php/deleteOrder.php
@@ -0,0 +1,22 @@
+<?php
+    include('connect_bd.php');
+
+    $id_pedido = $_GET['id_pedido'];
+    $query_delete = "DELETE FROM pedido WHERE id_pedido = '$id_pedido'";
+    $query_delete_details = "DELETE FROM detalles_pedidos WHERE id_pedido = '$id_pedido'";
+
+    $delete_details = mysqli_query($conexion, $query_delete_details);
+    $delete = mysqli_query($conexion, $query_delete);
+    
+    if($delete) {
+        echo "<script> alert('Se a eliminado al empleado exitosamente'); window.location='../pages/orders.php';</script>";
+    } else {
+        echo 
+        '<script> 
+            alert ("Algo salió mal"); window.history.go(-1);
+        </script>';
+    }
+?>
+
+
+
