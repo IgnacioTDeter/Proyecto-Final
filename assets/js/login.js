c71abb98b8be4d530f86075e033a05eb5f4c7463
Tobías Oviedo <76019160+Tobias388@users.noreply.github.com>
2023-10-24 17:49:20 -0300

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,10 +16,7 @@ form.addEventListener('submit', (e) => {
         if(data === 'error_inputs') { 
             msg.innerHTML = 'Asegurese que todos los campos estén llenos';
             msg.classList.add('active');
-        } else if (data === 'error_user_name') {
-            msg.innerHTML = 'Nombre de usuario o contraseña incorrecta';
-            msg.classList.add('active');
-        } else if (data === 'error_password') {
+        } else if (data === 'error_user') {
             msg.innerHTML = 'Nombre de usuario o contraseña incorrecta';
             msg.classList.add('active');
         } else if (data === 'success') {
