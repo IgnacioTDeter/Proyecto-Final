140499d12dc507d2dc22b30f1d1f7779ad48ea7d
DantePufahl <107288445+DantePufahl@users.noreply.github.com>
2023-11-12 14:19:07 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if(isset($_SESSION['user'])) {
               <label for="name" class="form__label">Contraseña</label>
             </div>
             
-            <a href="password_forgotten.php" class="forgo_pass"> ¿Olvidaste tu contraseña?</a>
+            <a href="./pages/password_forgotten.php" class="forgo_pass"> ¿Olvidaste tu contraseña?</a>
             <input type="submit" class="form__submit" value="Iniciar sesion">
           </div>
         </form>
